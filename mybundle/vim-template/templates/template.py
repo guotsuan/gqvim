d8ee83a6070bcd90c9180d4123a32cd713af93c3
Quan Guo <guotsuan@gmail.com>
2014-06-20 12:13:06 +0200

    add vim-template back as a sub-directory

---
+++ mybundle/vim-template/templates/template.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python2
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © %YEAR% %USER% <%MAIL%>
+#
+# Distributed under terms of the %LICENSE% license.
+#
+# Last modified: 2014 May 20
+
+"""
+%HERE%
+"""
---
+++ mybundle/vim-template/templates/template.sh
@@ -0,0 +1,25 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: %FFILE%
+# 
+#         USAGE: ./%FFILE%
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: %USER%@%HOST%, 
+#  ORGANIZATION: 
+#       CREATED: %FDATE%
+#      REVISION: ---
+# Last modified: 2014 Jun 12
+#
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+%HERE%
+
